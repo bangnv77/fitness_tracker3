718fd9175520899db15d81ad03961b02722913a8
Bang Nguyen <bangnv77@yahoo.com>
2016-09-22 18:59:22 -0700

    create Food and Exercise models, create database tables and view folders

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
+  resources :users do
+    resources :foods
+  end
+
+  resources :users do
+    resources :exercises
+  end
+
   root to: "application#index"
 end
